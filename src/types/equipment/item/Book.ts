bd4507d2507e01e363dad831a50f87e691ed6087
Lukas Obermann <lukas.obermann@icloud.com>
2025-10-17 13:19:31 +0200

    feat: books may have multiple cost values dependending on the edition or other factors

--- src/types/equipment/item/Book.ts
+++ src/types/equipment/item/Book.ts
@@ -1,4 +1,16 @@
-import { Entity, IncludeIdentifier, Object, Optional, Required, String } from "tsondb/schema/def"
+import {
+  Array,
+  Entity,
+  Enum,
+  EnumCase,
+  IncludeIdentifier,
+  Integer,
+  Object,
+  Optional,
+  Required,
+  String,
+  TypeAlias,
+} from "tsondb/schema/def"
 import { NestedTranslationMap } from "../../Locale.js"
 import { Errata } from "../../source/_Erratum.js"
 import { src } from "../../source/_PublicationRef.js"
@@ -11,7 +23,7 @@ export const Book = Entity(import.meta.url, {
     Object({
       cost: Required({
         comment: "The cost in silverthalers.",
-        type: IncludeIdentifier(Cost),
+        type: IncludeIdentifier(BookCost),
       }),
       weight: Optional({
         comment: "The weight in kg.",
@@ -62,4 +74,78 @@ export const Book = Entity(import.meta.url, {
       ),
     }),
   displayName: {},
+})
+
+const BookCost = Enum(import.meta.url, {
+  name: "BookCost",
+  values: () => ({
+    Constant: EnumCase({
+      comment: "The book has a fixed cost.",
+      type: Integer({ minimum: 0 }),
+    }),
+    Variable: EnumCase({
+      comment: "The book’s cost varies by edition or other factors.",
+      type: Array(IncludeIdentifier(BookCostVariant), { minItems: 2 }),
+    }),
+  }),
+})
+
+const BookCostVariant = Enum(import.meta.url, {
+  name: "BookCostVariant",
+  comment: "A variant for the book’s cost.",
+  values: () => ({
+    Definite: EnumCase({
+      comment: "A definite cost value.",
+      type: IncludeIdentifier(DefiniteBookCostVariant),
+    }),
+    Indefinite: EnumCase({
+      comment: "An indefinite cost value.",
+      type: IncludeIdentifier(IndefiniteBookCostVariant),
+    }),
+  }),
+})
+
+const DefiniteBookCostVariant = TypeAlias(import.meta.url, {
+  name: "DefiniteBookCostVariant",
+  comment: "A definite cost value for the book, such as a specific cost or cost range.",
+  type: () =>
+    Object({
+      cost: Required({
+        comment: "The cost in silverthalers.",
+        type: IncludeIdentifier(Cost),
+      }),
+      translations: NestedTranslationMap(
+        Required,
+        "DefiniteBookCostVariant",
+        Object({
+          label: Optional({
+            comment: "The label for the cost variant, e.g. “Original script”.",
+            type: String({ minLength: 1, isMarkdown: true }),
+          }),
+        })
+      ),
+    }),
+})
+
+const IndefiniteBookCostVariant = TypeAlias(import.meta.url, {
+  name: "DefiniteBookCostVariant",
+  comment:
+    "An indefinite cost value for the book, which means it is impossible to narrow the range of the cost value by any numbers.",
+  type: () =>
+    Object({
+      translations: NestedTranslationMap(
+        Required,
+        "DefiniteBookCostVariant",
+        Object({
+          label: Optional({
+            comment: "The label for the cost variant, e.g. “Original script”.",
+            type: String({ minLength: 1, isMarkdown: true }),
+          }),
+          description: Required({
+            comment: "The description of the cost variant, e.g. “Original script”.",
+            type: String({ minLength: 1, isMarkdown: true }),
+          }),
+        })
+      ),
+    }),
 })
