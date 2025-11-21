618d60d53867dace36c230632e03da646ae50509
Lukas Obermann <lukas.obermann@icloud.com>
2025-11-21 18:08:17 +0100

    docs: clarify master of aspect suffix property

--- src/types/Aspect.ts
+++ src/types/Aspect.ts
@@ -16,7 +16,7 @@ export const Aspect = Entity(import.meta.url, {
           }),
           master_of_aspect_suffix: Optional({
             comment:
-              "The aspect’s name appended to the simple name (not `name_in_library`) of the special ability *Master of (Aspect)*.",
+              "In some languages, the aspect’s grammatical gender influences the full name of a *Master of (Aspect)* instance. To make this possible, the *name* (**not** the *name in library*) is joined with what is contained in this field.",
             type: String({
               minLength: 1,
             }),
