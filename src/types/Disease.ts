da69a793a65e7737505c4ff140f6be2132119d6c
Lukas Obermann <lukas.obermann@icloud.com>
2025-11-27 19:41:37 +0100

    feat: add trade secret option for diseases

--- src/types/Disease.ts
+++ src/types/Disease.ts
@@ -1,6 +1,7 @@
-import { Entity, Object, Required } from "tsondb/schema/def"
+import { Entity, IncludeIdentifier, Object, Optional, Required } from "tsondb/schema/def"
 import { NestedTranslationMap } from "./Locale.js"
 import { cause, DiseaseTranslation, level, resistance } from "./_DiseasePoison.js"
+import { RecipeTradeSecret } from "./equipment/item/_Herbary.js"
 import { src } from "./source/_PublicationRef.js"
 
 export const Disease = Entity(import.meta.url, {
@@ -11,6 +12,10 @@ export const Disease = Entity(import.meta.url, {
       level,
       resistance,
       cause,
+      trade_secret: Optional({
+        comment: "AP value and prerequisites of the disease’s trade secret.",
+        type: IncludeIdentifier(RecipeTradeSecret),
+      }),
       src,
       translations: NestedTranslationMap(Required, "Disease", DiseaseTranslation),
     }),
