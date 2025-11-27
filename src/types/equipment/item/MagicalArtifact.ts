0a86d7396b2fddd977e214ab8be283dd31353292
Lukas Obermann <lukas.obermann@icloud.com>
2025-11-27 09:52:21 +0100

    feat: magical artifacts may define complexity and combat uses
    Closes #62

--- src/types/equipment/item/MagicalArtifact.ts
+++ src/types/equipment/item/MagicalArtifact.ts
@@ -1,6 +1,6 @@
 import { Entity, IncludeIdentifier, Object, Optional, Required } from "tsondb/schema/def"
 import { src } from "../../source/_PublicationRef.js"
-import { Cost, DefaultItemTranslations, Weight } from "./_Item.js"
+import { CombatUse, Complexity, Cost, DefaultItemTranslations, Weight } from "./_Item.js"
 
 export const MagicalArtifact = Entity(import.meta.url, {
   name: "MagicalArtifact",
@@ -14,6 +14,15 @@ export const MagicalArtifact = Entity(import.meta.url, {
       weight: Optional({
         comment: "The weight in kg.",
         type: IncludeIdentifier(Weight),
+      }),
+      complexity: Optional({
+        comment: "The complexity of crafting the item.",
+        type: IncludeIdentifier(Complexity),
+      }),
+      combat_use: Optional({
+        comment:
+          "The item can also be used either as an improvised weapon or as an armor, although this is not the primary use case of the item.",
+        type: IncludeIdentifier(CombatUse),
       }),
       src,
       translations: DefaultItemTranslations("MagicalArtifact"),
