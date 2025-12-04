6958dd7f7bda88027d38ba2a3de75a0c645ff7f7
Lukas Obermann <lukas.obermann@icloud.com>
2025-12-04 20:53:01 +0100

    feat!: add map and permanent cost options to magical signs
    Closes #75

--- src/types/specialAbility/MagicalSign.ts
+++ src/types/specialAbility/MagicalSign.ts
@@ -1,7 +1,18 @@
-import { Entity, IncludeIdentifier, Integer, Object, Optional, Required } from "tsondb/schema/def"
+import {
+  Entity,
+  Enum,
+  EnumCase,
+  IncludeIdentifier,
+  Integer,
+  Object,
+  Optional,
+  Required,
+  TypeAlias,
+} from "tsondb/schema/def"
 import { effect, name, name_in_library } from "../_Activatable.js"
 import { ap_value_append, ap_value_l10n } from "../_ActivatableAdventurePointsValue.js"
 import { propertyOptional } from "../_ActivatableNonMundane.js"
+import { CostMap } from "../_ActivatableSkillCost.js"
 import { GeneralPrerequisites } from "../_Prerequisite.js"
 import { NestedTranslationMap } from "../Locale.js"
 import { Errata } from "../source/_Erratum.js"
@@ -17,7 +28,7 @@ export const MagicalSign = Entity(import.meta.url, {
       }),
       cost: Optional({
         comment: "The cost in AE.",
-        type: Integer({ minimum: 0 }),
+        type: IncludeIdentifier(MagicalSignCost),
       }),
       property: propertyOptional(),
       ap_value: Required({
@@ -41,4 +52,27 @@ export const MagicalSign = Entity(import.meta.url, {
       ),
     }),
   displayName: {},
+})
+
+const MagicalSignCost = Enum(import.meta.url, {
+  name: "MagicalSignCost",
+  values: () => ({
+    Constant: EnumCase({ type: IncludeIdentifier(ConstantMagicalSignCost) }),
+    Map: EnumCase({ type: IncludeIdentifier(CostMap) }),
+  }),
+})
+
+const ConstantMagicalSignCost = TypeAlias(import.meta.url, {
+  name: "ConstantMagicalSignCost",
+  type: () =>
+    Object({
+      value: Required({
+        comment: "The AE cost value.",
+        type: Integer({ minimum: 1 }),
+      }),
+      permanent_value: Optional({
+        comment: "The part of the cost value that has to be spent permanently.",
+        type: Integer({ minimum: 1 }),
+      }),
+    }),
 })
