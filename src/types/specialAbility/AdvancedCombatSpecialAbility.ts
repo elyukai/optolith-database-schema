254f683dd219ba8dfefffb30568e7898cbebb974
Lukas Obermann <lukas.obermann@icloud.com>
2025-12-09 16:12:46 +0100

    feat: add input option to advanced combat special abilities
    Closes #80

--- src/types/specialAbility/AdvancedCombatSpecialAbility.ts
+++ src/types/specialAbility/AdvancedCombatSpecialAbility.ts
@@ -1,5 +1,5 @@
 import { Entity, IncludeIdentifier, Object, Optional, Required } from "tsondb/schema/def"
-import { levels, maximum, name, name_in_library, rules } from "../_Activatable.js"
+import { input, levels, maximum, name, name_in_library, rules } from "../_Activatable.js"
 import { ap_value, ap_value_append, ap_value_l10n } from "../_ActivatableAdventurePointsValue.js"
 import { combat_techniques, penalty, usage_type } from "../_ActivatableCombat.js"
 import { explicit_select_options, select_options } from "../_ActivatableSelectOptions.js"
@@ -35,6 +35,7 @@ export const AdvancedCombatSpecialAbility = Entity(import.meta.url, {
         Object({
           name,
           name_in_library,
+          input,
           rules,
           ap_value_append,
           ap_value: ap_value_l10n,
