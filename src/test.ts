05465711d2ea7274deda9ec2f03da7234021a1b6
Lukas Obermann <lukas.obermann@icloud.com>
2025-12-13 00:36:52 +0100

    test: display schema errors not as runtime errors

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,14 @@
+import { stderr } from "node:process"
+import { styleText } from "node:util"
+
 // importing the main file will run the schema checks
-import "./main.js"
+import("./main.js")
+  .then(() => {
+    console.log(styleText("green", "Schema is valid"))
+  })
+  .catch((err: unknown) => {
+    console.error(
+      styleText("red", err instanceof Error ? err.message : String(err), { stream: stderr }),
+    )
+    process.exitCode = 1
+  })
