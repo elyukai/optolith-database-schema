a60b193ef7de61ade1eeaf20e6781f49f4037891
Lukas Obermann <lukas.obermann@icloud.com>
2025-12-29 17:31:47 +0100

    style: make enum cases without value one-line

--- src/types/_SkillCheck.ts
+++ src/types/_SkillCheck.ts
@@ -15,23 +15,11 @@ export const SkillCheckPenalty = Enum(import.meta.url, {
   name: "SkillCheckPenalty",
   comment: "A specific value that represents a penalty for the associated skill check.",
   values: () => ({
-    Spirit: EnumCase({
-      type: null,
-    }),
-    HalfOfSpirit: EnumCase({
-      type: null,
-    }),
-    Toughness: EnumCase({
-      type: null,
-    }),
-    HigherOfSpiritAndToughness: EnumCase({
-      type: null,
-    }),
-    SummoningDifficulty: EnumCase({
-      type: null,
-    }),
-    CreationDifficulty: EnumCase({
-      type: null,
-    }),
+    Spirit: EnumCase({ type: null }),
+    HalfOfSpirit: EnumCase({ type: null }),
+    Toughness: EnumCase({ type: null }),
+    HigherOfSpiritAndToughness: EnumCase({ type: null }),
+    SummoningDifficulty: EnumCase({ type: null }),
+    CreationDifficulty: EnumCase({ type: null }),
   }),
 })
