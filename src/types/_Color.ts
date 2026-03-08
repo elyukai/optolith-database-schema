be9a69e70838fc455c212b592acf98629ab2ee8c
Lukas Obermann <lukas.obermann@icloud.com>
2026-03-08 17:24:09 +0100

    style: sort colors alphabetically

--- src/types/_Color.ts
+++ src/types/_Color.ts
@@ -4,14 +4,14 @@ export const AttributeColor = DB.Enum(import.meta.url, {
   name: "AttributeColor",
   comment: "A display color for an attribute.",
   values: () => ({
-    Red: DB.EnumCase({ type: null }),
-    Purple: DB.EnumCase({ type: null }),
-    Green: DB.EnumCase({ type: null }),
     Black: DB.EnumCase({ type: null }),
-    Yellow: DB.EnumCase({ type: null }),
     Blue: DB.EnumCase({ type: null }),
-    White: DB.EnumCase({ type: null }),
+    Green: DB.EnumCase({ type: null }),
     Orange: DB.EnumCase({ type: null }),
+    Purple: DB.EnumCase({ type: null }),
+    Red: DB.EnumCase({ type: null }),
+    White: DB.EnumCase({ type: null }),
+    Yellow: DB.EnumCase({ type: null }),
   }),
 })
 
@@ -20,11 +20,11 @@ export const GradientColor = DB.Enum(import.meta.url, {
   comment:
     "A display color for entries that have a certain color as a gradient in the header of their box in the official publications.",
   values: () => ({
-    TribalTeal: DB.EnumCase({ type: null }), // HEX #507471 / CMYK 55 13 33 48
-    OldRed: DB.EnumCase({ type: null }), // HEX #8c3537 / CMYK 51 91 81 6
-    Verdigris: DB.EnumCase({ type: null }), // HEX #5b6235 / CMYK 70 53 91 17
     Gaia: DB.EnumCase({ type: null }), // HEX #62934b / CMYK 64 17 82 13
-    SanMarino: DB.EnumCase({ type: null }), // HEX #4f70a8 / CMYK 75 53 10 2
     LightHarvestGold: DB.EnumCase({ type: null }), // HEX #dbb578 / CMYK 9 26 56 9
+    OldRed: DB.EnumCase({ type: null }), // HEX #8c3537 / CMYK 51 91 81 6
+    SanMarino: DB.EnumCase({ type: null }), // HEX #4f70a8 / CMYK 75 53 10 2
+    TribalTeal: DB.EnumCase({ type: null }), // HEX #507471 / CMYK 55 13 33 48
+    Verdigris: DB.EnumCase({ type: null }), // HEX #5b6235 / CMYK 70 53 91 17
   }),
 })
